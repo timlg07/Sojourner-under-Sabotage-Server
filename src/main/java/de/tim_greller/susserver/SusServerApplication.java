e318f31cae6bd0ae7a89f0f0562f87a24eaa4eea
Tim Greller <timlg07@gmail.com>
2023-09-20 13:17:10 +0200

    init spring project

---
+++ src/main/java/de/tim_greller/susserver/SusServerApplication.java
@@ -0,0 +1,13 @@
+package de.tim_greller.susserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SusServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SusServerApplication.class, args);
+    }
+
+}
