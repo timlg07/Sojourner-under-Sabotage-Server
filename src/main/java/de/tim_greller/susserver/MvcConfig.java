5a3661c3d184f4fdf2522bf23911d2765ac3e766
Tim Greller <timlg07@gmail.com>
2023-09-24 23:49:59 +0200

    move mvc config out of security package

--- src/main/java/de/tim_greller/susserver/MvcConfig.java
+++ src/main/java/de/tim_greller/susserver/MvcConfig.java
@@ -1,4 +1,4 @@
-package de.tim_greller.susserver.security;
+package de.tim_greller.susserver;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,9 +10,9 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/hello").setViewName("hello");
-        registry.addViewController("/").setViewName("home");
         registry.addViewController("/login").setViewName("login");
     }
 
