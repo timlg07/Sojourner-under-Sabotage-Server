2c6eb46f249f455f6016d6ccc04f89ff2ec58bc9
Tim Greller <timlg07@gmail.com>
2023-10-09 20:00:27 +0200

    add tipp to test exec timeout error

--- src/main/java/de/tim_greller/susserver/exception/TestExecutionTimedOut.java
+++ src/main/java/de/tim_greller/susserver/exception/TestExecutionTimedOut.java
@@ -2,6 +2,6 @@ package de.tim_greller.susserver.exception;
 
 public class TestExecutionTimedOut extends TestExecutionException {
     public TestExecutionTimedOut(int seconds) {
-        super("Test execution timed out after " + seconds + " seconds");
+        super("Test execution timed out after " + seconds + " seconds. Maybe you created an infinite loop?");
     }
 }
