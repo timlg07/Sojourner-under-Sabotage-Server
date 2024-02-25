a8864c2d5499fcc701acc674e4808f26da1d74b8
Tim Greller <timlg07@gmail.com>
2024-02-25 14:41:22 +0100

    generic event send/receive via STOMP, jackson

--- src/main/java/de/tim_greller/susserver/controller/ws/EventController.java
+++ src/main/java/de/tim_greller/susserver/controller/ws/EventController.java
@@ -1,6 +1,7 @@
 package de.tim_greller.susserver.controller.ws;
 
 import de.tim_greller.susserver.events.Event;
+import de.tim_greller.susserver.events.RoomUnlockedEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -12,10 +13,10 @@ public class EventController {
 
     @MessageMapping("/events")  // complete endpoint depends on configured application message handler prefix, e.g. /app/hello
     @SendTo("/topic/events")
-    public Event greeting(Event clientEvent) throws Exception {
-        log.info("client event timestamp: {}", clientEvent.getTimestamp());
+    public Event handleClientEvent(Event clientEvent) throws Exception {
+        log.info("client event [{}] timestamp: {}", clientEvent.getClass(), clientEvent.getTimestamp());
         Thread.sleep((int)(Math.random()*2000)); // simulated delay
-        var serverEvent = new Event();
+        var serverEvent = new RoomUnlockedEvent(1L);
         log.info("server event timestamp: {}", serverEvent.getTimestamp());
         return serverEvent;
     }
--- src/main/java/de/tim_greller/susserver/events/Event.java
+++ src/main/java/de/tim_greller/susserver/events/Event.java
@@ -1,8 +1,10 @@
 package de.tim_greller.susserver.events;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.Getter;
 
 @Getter
+@JsonTypeInfo(use = JsonTypeInfo.Id.MINIMAL_CLASS, property = "type")
 public class Event {
     private final long timestamp = System.currentTimeMillis();
 }
