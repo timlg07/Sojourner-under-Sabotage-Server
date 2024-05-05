cd3b7c4d239c0548db36a8776dfadb8944fd0745
Tim Greller <timlg07@gmail.com>
2024-05-05 21:32:30 +0200

    add RnaAnalyzer CUT & test

---
+++ src/main/resources/cut/RnaAnalyzer.java
@@ -0,0 +1,59 @@
+/**
+ * This component helps detecting viruses and their mutations based
+ * on RNA (Ribonucleic acid) data.
+ * The data are strings over the four-letter alphabet {A, C, G, U}.
+ */
+public class RnaAnalyzer {
+
+    String virusRnaSequence;
+
+    public RnaAnalyzer(String virusRnaSequence) {
+        this.virusRnaSequence = stripNonRnaChars(virusRnaSequence);
+    }
+
+    /**
+     * Checks what the longest common subsequence of the virus RNA sequence
+     * and the given RNA data is.
+     *
+     * @param rnaData The RNA data to compare with the virus RNA sequence.
+     *                It may contain non-RNA characters which will be ignored.
+     * @return The longest common subsequence of the virus RNA sequence and the given RNA data.
+     */
+    public int longestCommonRnaSubsequence(String rnaData) {
+        String rnaDataCleaned = stripNonRnaChars(rnaData);
+        return longestCommonRnaSubsequence(
+                virusRnaSequence, rnaDataCleaned,
+                virusRnaSequence.length(), rnaDataCleaned.length()
+        );
+    }
+
+    private String longestCommonRnaSubsequence(String rna1, String rna2, int len1, int len2) {
+        System.out.println("Compare " + rna1.substring(0, len1) + "\n  with " + rna2.substring(0, len2));
+
+        if (len1 == 0 || len2 == 0) {
+            return "";
+        }
+
+        if (rna1.charAt(len1 - 1) == rna2.charAt(len2 - 1)) {
+            return longestCommonRnaSubsequence(rna1, rna2, len1 - 1, len2 - 1) + rna1.charAt(len1 - 1);
+        } else {
+            String s1 = longestCommonRnaSubsequence(rna1, rna2, len1 - 1, len2);
+            String s2 = longestCommonRnaSubsequence(rna1, rna2, len1, len2 - 1);
+            System.out.println(" -> result: " + s1 + " vs " + s2);
+            return s1.length() > s2.length() ? s1 : s2;
+        }
+    }
+
+    /**
+     * Strips all non-RNA characters from the given string.
+     * RNA data consists of:
+     * A - adenine,
+     * C - cytosine,
+     * G - guanine,
+     * U - uracil.
+     */
+    private String stripNonRnaChars(String rnaData) {
+        return rnaData.toUpperCase().replaceAll("[^ACGU]", "");
+    }
+
+}
---
+++ src/main/resources/test/stage-1/RnaAnalyzerTest.java
@@ -0,0 +1,59 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/* For assertions you can use:
+ - assertTrue
+ - assertFalse
+ - assertEquals
+ - assertNotEquals
+ - assertArrayEquals
+ - assertThrows
+ - assertNull
+ - assertNotNull
+ */
+
+public class RnaAnalyzerTest {
+
+    @Test
+    public void hiddenTest1() {
+        RnaAnalyzer analyzer = new RnaAnalyzer("AAGC");
+        assertEquals("AG", analyzer.longestCommonRnaSubsequence("AGUA"));
+    }
+
+    @Test
+    public void hiddenTest2() {
+        RnaAnalyzer analyzer = new RnaAnalyzer("AAGAUGCCGU");
+        assertEquals("AUGCCGU", analyzer.longestCommonRnaSubsequence("AAGCCGAUGCCGUGCC"));
+    }
+
+    @Test
+    public void testStripCharacters1() {
+        RnaAnalyzer analyzer = new RnaAnalyzer("AA");
+        assertEquals("AA", analyzer.longestCommonRnaSubsequence("AHello World!A"));
+    }
+
+    @Test
+    public void testStripCharacters2() {
+        RnaAnalyzer analyzer = new RnaAnalyzer("A Test A");
+        assertEquals("AA", analyzer.longestCommonRnaSubsequence("AA"));
+    }
+
+    @Test
+    public void testNoCommonSubsequence1() {
+        RnaAnalyzer analyzer = new RnaAnalyzer("AAGAUGCCGU");
+        assertEquals("", analyzer.longestCommonRnaSubsequence(""));
+    }
+
+    @Test
+    public void testNoCommonSubsequence2() {
+        RnaAnalyzer analyzer = new RnaAnalyzer("AAG");
+        assertEquals("", analyzer.longestCommonRnaSubsequence("CUC"));
+    }
+
+    @Test
+    public void testMultipleLongCommonSubsequences() {
+        RnaAnalyzer analyzer = new RnaAnalyzer("AAGAUGCCGU");
+        assertEquals("CGU", analyzer.longestCommonRnaSubsequence("CGAGAAACGUACC"));
+    }
+
+}
